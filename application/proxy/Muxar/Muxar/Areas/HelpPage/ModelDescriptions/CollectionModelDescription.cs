f8ad08dd8a12b787dfb22da73357629079860396
elena.creanga <elena.creanga.mail@gmail.com>
2016-01-24 17:16:16 +0200

    add proxy - used to merge different datasets

---
+++ application/proxy/Muxar/Muxar/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Muxar.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ application/proxy/Muxar/Muxar/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Muxar.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
