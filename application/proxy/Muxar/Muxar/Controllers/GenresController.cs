dc748c1696efaf5d455cee516fe94c85c751f027
elena.creanga <elena.creanga.mail@gmail.com>
2016-01-27 02:47:27 +0200

    add endpoint for genres

---
+++ application/proxy/Muxar/Muxar/Controllers/GenresController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Muxar.BrightStarDb.Endpoints;
+
+namespace Muxar.Controllers
+{
+    public class GenresController : ApiController
+    {
+        private DbpediaEndpoint dbpediaEndpoint;
+
+        public GenresController()
+        {
+            dbpediaEndpoint = new DbpediaEndpoint();
+        }
+
+        public IHttpActionResult GetByArtist(string artistLabel)
+        {
+            var result = dbpediaEndpoint.GetGenresByArtist(artistLabel);
+            return Ok(result);
+        }
+    }
+}
